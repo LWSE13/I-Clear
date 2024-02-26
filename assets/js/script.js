26151c9ac36acbf14818f685d158ca32414053a0
LWSE13 <leewheeler888@gmail.com>
2024-02-26 18:19:56 +0000

    added functionality to make recent searches appear undeer the search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
-searchBtnEl = document.getElementById("search-button");
-searchInputEl = document.getElementById("search-input");
+var searchBtnEl = document.getElementById("search-button");
+var searchInputEl = document.getElementById("search-input");
+var historyButtonEl = document.getElementById("clear-history");
+var recentSearchContainerEl = document.getElementById("button-container");
+
 
 searchBtnEl.addEventListener("click", function() {
-    if (searchInputEl === "") {
+    if (searchInputEl.value === "") {
         alert("Please enter a search term")
         return;
     }
     var recentSearch = searchInputEl.value;
+    var newButton = document.createElement("button");
+    newButton.textContent = recentSearch;
+    recentSearchContainerEl.appendChild(newButton);
     var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
     searchHistory.push(recentSearch);
+    searchInputEl.value = "";
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+})
+
+historyButtonEl.addEventListener("click", function() {
+    localStorage.clear();
 })
