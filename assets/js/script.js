e1b1f4652d65ac49b2a6b679dac6ba45ecbd28cf
LWSE13 <leewheeler888@gmail.com>
2024-02-27 17:01:00 +0000

    begun work on api fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var searchBtnEl = document.getElementById("search-button");
 var searchInputEl = document.getElementById("search-input");
 var historyButtonEl = document.getElementById("clear-history");
 var recentSearchContainerEl = document.getElementById("button-container");
+const apiBase = "https://api.openweathermap.org";
+const apiKey = "f23ee9deb4e1a7450f3157c44ed020e1";
 
 
 searchBtnEl.addEventListener("click", function() {
@@ -17,6 +19,16 @@ searchBtnEl.addEventListener("click", function() {
     searchHistory.push(recentSearch);
     searchInputEl.value = "";
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+
+    var query = `${apiBase}/data/2.5/weather?q=${recentSearch}&appid=${apiKey}`;
+    fetch(query)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            // update HTML with weather data
+        });
 })
 
 historyButtonEl.addEventListener("click", function() {
