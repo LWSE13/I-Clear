621cf9267663e08a7b5c49a26cd8c7651278a141
LWSE13 <leewheeler888@gmail.com>
2024-02-28 04:26:24 +0000

    added code that stops button duplication. restructured js so that function is defined before being called. added parameters to weather search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,22 @@ var searchBtnEl = document.getElementById("search-button");
 var searchInputEl = document.getElementById("search-input");
 var historyButtonEl = document.getElementById("clear-history");
 var recentSearchContainerEl = document.getElementById("button-container");
+
 const apiBase = "https://api.openweathermap.org";
 const apiKey = "f23ee9deb4e1a7450f3157c44ed020e1";
 
-searchBtnEl.addEventListener("click", function() {
-    if (searchInputEl.value === "") {
+// Function to search for weather. added location and recentButtonCheck as parameters to prevent button duplication
+function searchForWeather(location, recentButtonCheck) {
+    
+    // Check if location is empty and prompts user to enter a search term
+    if (location === "") {
         alert("Please enter a search term")
         return;
     }
-    var recentSearch = searchInputEl.value;
 
-    var query = `${apiBase}/data/2.5/weather?q=${recentSearch}&appid=${apiKey}`;
-    fetch(query)
+    // Fetch request to Openweather API. Used backticks to insert my api info and location variable (when it is defined by the user input)
+    var weatherRequest = `${apiBase}/data/2.5/weather?q=${location}&appid=${apiKey}`;
+    fetch(weatherRequest)
         .then(function(response) {
             if (!response.ok) {
                 console.log('Error: Location not found');
@@ -27,17 +31,34 @@ searchBtnEl.addEventListener("click", function() {
                 console.log(data);
                 // TODO: update HTML with weather data
 
-                var newButton = document.createElement("button");
-                newButton.textContent = recentSearch;
-                recentSearchContainerEl.appendChild(newButton);
-                var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-                searchHistory.push(recentSearch);
+                if (recentButtonCheck) {
+                    var recentSearchButtonEl = document.createElement("button");
+                    recentSearchButtonEl.textContent = location;
+                    recentSearchButtonEl.classList.add ("button-styling");
+                    recentSearchButtonEl.addEventListener("click", function() {
+                        searchForWeather(location, false);
+                    });
+                    recentSearchContainerEl.appendChild(recentSearchButtonEl);
+                    var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+                    searchHistory.push(location);
+                    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+                }
                 searchInputEl.value = "";
-                localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
             }
         });
+}
+
+// Event listener for search button
+searchBtnEl.addEventListener("click", function() {
+    var searchValue = searchInputEl.value;
+    searchForWeather(searchValue, true);
 })
 
+// Event listener for history button
 historyButtonEl.addEventListener("click", function() {
     localStorage.clear();
 })
+
+// Load recent searches from local storage
+
+// Display API data on page
