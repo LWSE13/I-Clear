8678153075233e3c025bd564e529b2c3ce896b19
LWSE13 <leewheeler888@gmail.com>
2024-02-28 03:07:28 +0000

    added error handling to my fetch request. Restructured JS in order to have my code return if an error is detected

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,29 +5,36 @@ var recentSearchContainerEl = document.getElementById("button-container");
 const apiBase = "https://api.openweathermap.org";
 const apiKey = "f23ee9deb4e1a7450f3157c44ed020e1";
 
-
 searchBtnEl.addEventListener("click", function() {
     if (searchInputEl.value === "") {
         alert("Please enter a search term")
         return;
     }
     var recentSearch = searchInputEl.value;
-    var newButton = document.createElement("button");
-    newButton.textContent = recentSearch;
-    recentSearchContainerEl.appendChild(newButton);
-    var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-    searchHistory.push(recentSearch);
-    searchInputEl.value = "";
-    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 
     var query = `${apiBase}/data/2.5/weather?q=${recentSearch}&appid=${apiKey}`;
     fetch(query)
         .then(function(response) {
+            if (!response.ok) {
+                console.log('Error: Location not found');
+                alert('Error: Location not found');
+                return;
+            }
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
-            // update HTML with weather data
+            if (data) {
+                console.log(data);
+                // TODO: update HTML with weather data
+
+                var newButton = document.createElement("button");
+                newButton.textContent = recentSearch;
+                recentSearchContainerEl.appendChild(newButton);
+                var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+                searchHistory.push(recentSearch);
+                searchInputEl.value = "";
+                localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+            }
         });
 })
 
