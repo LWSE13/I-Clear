583458d3461affea5f19be796933ba65d9984558
LWSE13 <leewheeler888@gmail.com>
2024-02-29 05:26:21 +0000

    completeed

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var dateEl = document.getElementById("date");
 var mainTempEl = document.getElementById("main-temp");
 var mainHumidityEl = document.getElementById("main-humidity");
 var mainWindEl = document.getElementById("main-wind");
-
+var forecastContainer = document.getElementById("bottom-container");
 const apiBase = "https://api.openweathermap.org";
 const apiKey = "f23ee9deb4e1a7450f3157c44ed020e1";
 
@@ -31,20 +31,19 @@ function loadSearchHistory () {
 loadSearchHistory();
 // Function to search for weather. added location and recentButtonCheck as parameters to prevent button duplication
 function searchForWeather(location, recentButtonCheck) {
-    
     // Check if location is empty and prompts user to enter a search term
     if (location === "") {
-        alert("Please enter a search term")
+        alert("Please enter a search term");
         return;
     }
-
+    forecastContainer.innerHTML = "";
     // Fetch request to Openweather API. Used backticks to insert my api info and location variable (when it is defined by the user input line 55)
-    var weatherRequest = `${apiBase}/data/2.5/forecast?q=${location}&appid=${apiKey}`;
+    var weatherRequest = `${apiBase}/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`;
     fetch(weatherRequest)
         .then(function(response) {
             if (!response.ok) {
-                console.log('Error: Location not found');
-                alert('Error: Location not found');
+                console.log("Error: Location not found");
+                alert("Error: Location not found");
                 return;
             }
             return response.json();
@@ -55,29 +54,64 @@ function searchForWeather(location, recentButtonCheck) {
                 // TODO: update HTML with weather data
                 var cityName = data.city.name;
                 var currentDate = data.list[0].dt_txt.split(" ")[0];
+
                 var mainTemp = data.list[0].main.temp;
                 var mainHumidity = data.list[0].main.humidity;
                 var mainWind = data.list[0].wind.speed;
+
                 var weatherIconCode = data.list[0].weather[0].icon;
                 var weatherIconUrl = `http://openweathermap.org/img/w/${weatherIconCode}.png`;
 
+                var forecastData = data.list;
+                var dataPerDay = forecastData.filter(function(dataPosition) {
+                    var time = dataPosition.dt_txt.split(" ")[1];
+                    return time === "12:00:00";
+                });
+                console.log(dataPerDay);
+                dataPerDay.forEach(function(dataPosition) {
+                    // Created a new div for the iterated day's weather data
+                    var dayDiv = document.createElement("div");
+
+                    // Created HTML elements for each piece of data
+                    var dateEl = document.createElement("p");
+                    var tempEl = document.createElement("p");
+                    var humidityEl = document.createElement("p");
+                    var windEl = document.createElement("p");
+                    var iconEl = document.createElement("img");
+
+                    // Set the text content of each element
+                    dateEl.textContent = "Date: " + dataPosition.dt_txt.split(" ")[0];
+                    tempEl.textContent = "Temperature: " + dataPosition.main.temp;
+                    humidityEl.textContent = "Humidity: " + dataPosition.main.humidity;
+                    windEl.textContent = "Wind Speed: " + dataPosition.wind.speed;
+                    iconEl.src = `http://openweathermap.org/img/w/${dataPosition.weather[0].icon}.png`;
+
+                    // Appended each element to the day div
+                    dayDiv.appendChild(dateEl);
+                    dayDiv.appendChild(iconEl);
+                    dayDiv.appendChild(tempEl);
+                    dayDiv.appendChild(humidityEl);
+                    dayDiv.appendChild(windEl);
+
+                    // Appended the day div to the forecast container
+                    forecastContainer.appendChild(dayDiv);
+                });
+
                 cityHeadingEl.textContent = cityName;
                 dateEl.textContent = currentDate;
-                mainTempEl.textContent = "Temperature: " + mainTemp;
-                mainHumidityEl.textContent = "Humidity: " + mainHumidity;
-                mainWindEl.textContent = "Wind Speed: " + mainWind;
-                var weatherIconEl = document.createElement('img');
+
+                mainTempEl.textContent = "Temperature: " + mainTemp + " °C";
+                mainHumidityEl.textContent = "Humidity: " + mainHumidity + "%";
+                mainWindEl.textContent = "Wind Speed: " + mainWind + " m/s";
+
+                var weatherIconEl = document.createElement("img");
                 weatherIconEl.src = weatherIconUrl;
                 cityHeadingEl.appendChild(weatherIconEl);
-
-                
-
-
 
                 if (recentButtonCheck) {
                     var recentSearchButtonEl = document.createElement("button");
                     recentSearchButtonEl.textContent = location;
-                    recentSearchButtonEl.classList.add ("button-styling");
+                    recentSearchButtonEl.classList.add("button-styling");
                     recentSearchButtonEl.addEventListener("click", function() {
                         searchForWeather(location, false);
                     });
